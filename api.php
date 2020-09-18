518385ebb7166f7ec7e59a0ecd6796f2bbd9276a
MC Naveen <funmemcn@gmail.com>
2020-09-18 20:36:26 +0530

    ☮️ Added Functionality to Randomize User-Agent

--- api.php
+++ api.php
@@ -1,8 +1,5 @@
 <?php
 
-header('X-Frame-Options: DENY');
-header('X-XSS-Protection: 1; mode=block');
-header('X-Content-Type-Options: nosniff');
 header('Content-type:application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET');
@@ -36,7 +33,7 @@ try
     curl_setopt( $ch, CURLOPT_ENCODING, 'gzip');
     curl_setopt( $ch, CURLOPT_TIMEOUT, 20);
     curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, 5);
-    curl_setopt( $ch, CURLOPT_USERAGENT, "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36");
+    curl_setopt( $ch, CURLOPT_USERAGENT, getRandomUserAgent());
     curl_setopt( $ch, CURLOPT_HEADER, false);
     curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true);
     $data = curl_exec($ch);
@@ -86,4 +83,22 @@ function getSDLink($curl_content)
     {
         return false;
     }
+}
+
+function getRandomUserAgent()
+{
+ $userAgents=array(
+ "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6",
+ "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)",
+ "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)",
+ "Opera/9.20 (Windows NT 6.0; U; en)",
+ "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36",
+ "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; en) Opera 8.50",
+ "Mozilla/4.0 (compatible; MSIE 6.0; MSIE 5.5; Windows NT 5.1) Opera 7.02 [en]",
+ "Mozilla/5.0 (Macintosh; U; PPC Mac OS X Mach-O; fr; rv:1.7) Gecko/20040624 Firefox/0.9",
+ "Mozilla/5.0 (Macintosh; U; PPC Mac OS X; en) AppleWebKit/48 (like Gecko) Safari/48" 
+ );
+ $random = rand(0,count($userAgents)-1);
+ 
+ return $userAgents[$random];
 }
